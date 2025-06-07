cf776b6793848bf1a39b90c5ff0233018c1460de
Laurids33 <laurids.dechow@gmail.com>
2025-06-07 12:44:00 +0200

    Spawn defense

--- Assets/Scripts/Ziel.cs
+++ Assets/Scripts/Ziel.cs
@@ -5,6 +5,9 @@ public class Ziel : MonoBehaviour
     public GameObject jaeger;
     Jaeger jaegerKlasse;
 
+    public GameObject abwehrPrefab;
+    bool nachladen = false;
+
     void Start()
     {
         transform.position = new Vector3(Random.Range(20, 980), 50, Random.Range(20, 980));
@@ -13,6 +16,23 @@ public class Ziel : MonoBehaviour
 
     void Update()
     {
+        Vector3 abstand = jaeger.transform.position - transform.position;
+        if (!nachladen && abstand.magnitude < 10)
+        {
+            GameObject abwehr = (GameObject)Instantiate(
+                abwehrPrefab,
+                transform.position,
+                Quaternion.identity
+            );
+            abwehr.GetComponent<Rigidbody>().AddForce(100 * abstand);
+            nachladen = true;
+            Invoke(nameof(NachladenBeendet), 1);
+        }
         jaegerKlasse.Pruefen(gameObject);
+    }
+
+    void NachladenBeendet()
+    {
+        nachladen = false;
     }
 }
